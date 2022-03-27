888a6d3095785c5a76450895ba281fa97bd90c37
Falconiere R. Barbosa <falconieer@gmail.com>
2022-03-27 03:06:46 -0300

    Adding google api place requests

--- src/domains/main/SearchDirections/index.tsx
+++ src/domains/main/SearchDirections/index.tsx
@@ -1,10 +1,16 @@
+import { useGooglePlaces } from "apis/useGooglePlaces";
 import { Box, Input } from "native-base";
 import React from "react";
 
 export function SearchDirections() {
+  const { searchBy, results } = useGooglePlaces();
+
   return (
     <Box position="absolute" p="4" zIndex="1" w="100%" safeAreaTop>
-      <Input placeholder="Para vai sua mercadoria?" />
+      <Input
+        placeholder="Para vai sua mercadoria?"
+        onChangeText={text => searchBy(text)}
+      />
     </Box>
   );
 }
---
+++ src/helpers/constants.ts
@@ -0,0 +1,3 @@
+import Config from "react-native-config";
+
+export const GOOGLE_MAPS_API_KEY: string = Config.GOOGLE_MAPS_API_KEY;
